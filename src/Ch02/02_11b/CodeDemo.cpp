4b1c91a8b4533f1583d3b88654ea722a9f439a46
Thierry Florent Dzouato Djeumen <thierrydbspace@gmail.com>
2025-10-04 17:53:02 +0000

    more examples on casting

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -9,7 +9,11 @@ int main(){
     int fahrenheit = 100;
     int celsius;
 
-    celsius = fahrenheit;
+    //Alle Methoden sind korrekt 😊
+    //celsius = (float)(fahrenheit - 32) * 5 / 9; // C = (F - 32) * 5/9
+    //celsius = static_cast<float>(fahrenheit - 32) * 5 / 9;
+    //celsius = (fahrenheit - 32) * 5.0 / 9;
+    celsius = (fahrenheit - 32) * 5 / 9.0;
 
     std::cout << std::endl;
     std::cout << "Fahrenheit: " << fahrenheit << std::endl;
@@ -19,8 +23,12 @@ int main(){
     
     std::cout << std::endl;
     std::cout << "Float          : " << weight << std::endl;
-    std::cout << "Integer part   : " << weight << std::endl;
-    std::cout << "Fractional part: " << weight << std::endl;
+    std::cout << "Integer part   : " << static_cast<int>(weight) << std::endl;//the two gives 
+    std::cout << "Integer part   : " << (int)(weight) << std::endl;//the same result
+
+    std::cout << "Fractional part: " << (int)((weight - (int)(weight))*10000) << std::endl;//the two gives
+    std::cout << "Fractional part: " << static_cast<int>((weight - (int)(weight))*10000) << std::endl;//the same result
+
 
     std::cout << std::endl << std::endl;
     return 0;
