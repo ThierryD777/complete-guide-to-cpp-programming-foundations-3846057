e13de92711e0499e0bc89458c7f10d5a58d71b5a
Thierry Florent Dzouato Djeumen <thierrydbspace@gmail.com>
2025-10-04 05:31:37 +0000

    using variables1

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,8 +4,14 @@
 
 #include <iostream>
 
+int a, b = 12, c = 7, d; // Global variables accessible to all parts of the code
 int main(){
-    
+    bool myCondition = true; // Local variable accessible only within main()
+    a = 23, d = 98; // Assign values to previously declared global variables
+
+    std::cout << "The value of a is " << a << " and the value of b is " << b << "." << std::endl;
+    std::cout << "I am in " << (myCondition ? "good" : "not so good") << " conditions for the upcoming game and had an AR of " << d << "%." << std::endl;
+
     std::cout << std::endl << std::endl;
     return 0;
 }
