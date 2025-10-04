f496ee5fa4c45d782451c9fc3f3598609fd57ace
Thierry Florent Dzouato Djeumen <thierrydbspace@gmail.com>
2025-10-04 14:00:00 +0000

    structs and their uses

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -6,8 +6,19 @@
 #include <string>
 
 enum class character_role {protagonist, antagonist, sidekick, npc};
+struct game_characters{
+    std::string name;
+    int level;
+    character_role role;
+};
 
 int main(){
+    game_characters player1;
+    player1.name = "Andrew";
+    player1.level = 13;
+    player1.role = character_role::protagonist;
+    
+    std::cout << player1.name << " is a level " << player1.level << " character with the role of " << (int)player1.role << "." << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
