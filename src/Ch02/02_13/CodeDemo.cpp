aba88d254bd2220a8b1787d35990258494efd6ad
Thierry Florent Dzouato Djeumen <thierrydbspace@gmail.com>
2025-10-04 18:56:56 +0000

    my solutions to CH02_13

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -5,12 +5,13 @@
 #include <iostream>
 #include <cstdint>
 
-int add_int(float a, double b, long double c){
+int add_int(float a, double b, long double c){//but I find this approach wrong. I prefer adding all and cast at the end to avoid lost of values. Another approach is shown later
     int result = 0;
-
     // Write your code here
-    
-    return result;
+    return (int)a+(int)b+(int)c;
+}
+int addInt(float a, double b, long double c){//this is another approach
+    return static_cast<int>(a+b+c);
 }
 
 int main(){
@@ -19,8 +20,10 @@ int main(){
     long double c = 4.6;
 
     int learnerResult = add_int(a, b, c);
+    int anotherResult = addInt(a, b, c);
     
     std::cout << "Your code returned: " << learnerResult << std::endl;
+    std::cout << "Your other approach returned: " << anotherResult << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
