8fdc0783f170afa16126cebdc39d155dd1d4e8d6
Thierry Florent Dzouato Djeumen <thierrydbspace@gmail.com>
2025-10-05 00:16:11 +0000

    pointers

--- src/Ch04/04_02b/CodeDemo.cpp
+++ src/Ch04/04_02b/CodeDemo.cpp
@@ -9,13 +9,13 @@ int main(){
     int a = 37;
     int *ptr;
 
-    ptr = nullptr;
+    ptr = &a; //nullptr;//nullptr is a const of its own type, it is not 0, it is not NULL, it is nullptr. It represents a null location in memory.
 
-    std::cout << "             The value of a is " << ptr << std::endl;
+    std::cout << "             The value of a is " << a << std::endl;
     std::cout << "    ptr is pointing to address " << ptr << std::endl;
-    std::cout << "           The address of a is " << ptr << std::endl;
-    std::cout << "Where ptr is pointing, we have " << ptr << std::endl;
-    std::cout << "         The address of ptr is " << ptr << std::endl;
+    std::cout << "           The address of a is " << &a << std::endl;
+    std::cout << "Where ptr is pointing, we have " << *ptr << std::endl;
+    std::cout << "         The address of ptr is " << &ptr << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
