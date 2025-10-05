335f0b1b00258031ae719e3a341349f6afd145cb
Thierry Florent Dzouato Djeumen <thierrydbspace@gmail.com>
2025-10-05 17:52:52 +0000

    Using obejcts with vectors (complex numbers)

--- src/Ch04/04_07b/CodeDemo.cpp
+++ src/Ch04/04_07b/CodeDemo.cpp
@@ -7,6 +7,17 @@
 #include <complex>
 
 int main(){
+    std::vector<std::complex<double>> points;
+
+    points.push_back(std::complex<double>(1.0, 2.0));
+    points.push_back(std::complex<double>(3.0, -4.0));
+    points.push_back(std::complex<double>(-5.0, 6.0));
+    points.push_back(std::complex<double>(7.5, 8.3));
+
+    std::cout << "The first real part: " << points.begin()->real() << std::endl;// same as points[0].real()
+    std::cout << "The imaginary part at the 2nd index is: " << points[1].imag() << std::endl;// same as (points.begin() + 2)->imag()
+    std::cout << "Next to last real: " << prev(points.end(), 2)->real() << std::endl;// same as points[points.size() - 2].real()
+    std::cout << "Last imaginary: " << (points.end() - 1)->imag() << std::endl;// same as points[points.size() - 1].imag()
     
     std::cout << std::endl << std::endl;
     return 0;
